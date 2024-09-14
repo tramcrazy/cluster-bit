b37a469472a26bdc986fc9496b91acebda5dd8ef
tramcrazy <81013186+tramcrazy@users.noreply.github.com>
2024-09-14 19:33:21 +0100

    Create serial test code and initialise test task file

---
+++ microbit_code/serial_test.py
@@ -0,0 +1,7 @@
+from microbit import *
+from utime import sleep
+uart.init(baudrate=115200)
+uart.write("testing serial, hello!\n")
+sleep(10)
+received = uart.read()
+uart.write(str(received, "utf-8") + "RECEIVED SUCCESSFULLY\n")
