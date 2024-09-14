b6d0e55ebcd42bffa0e627a344c3d403d3624e75
tramcrazy <tram@tramcrazy.com>
2024-09-14 20:41:02 +0100

    Change to multiline strings, initialise micro:bit main code file

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ for port in ports:
         microbit_name = port.name # store friendlier name for later
         microbit_num += 1
 if microbit_num == 0: # troubleshooting advice when no micro:bit is connected
-    print("It seems like your micro:bit isn't connected to this computer.\nMake sure it's connected with a USB data cable and run cluster:bit again.")
+    print("""It seems like your micro:bit isn't connected to this computer.
+    Make sure it's connected with a USB data cable and run cluster:bit again.""")
     sys.exit()
 if microbit_num > 1: # troubleshooting advice when more than one micro:bit is connected
-    print("It seems like more than one micro:bit is connected to your computer.\ncluster:bit is designed to work with just one micro:bit over USB, and the others over radio.\nTry disconnecting any other micro:bits and run cluster:bit again.")
+    print("""It seems like more than one micro:bit is connected to your computer.
+    cluster:bit is designed to work with just one micro:bit over USB, and the others over radio.
+    Try disconnecting any other micro:bits and run cluster:bit again.""")
 print("A micro:bit has been detected on port " + microbit_name + ". Let's get clustering!")
