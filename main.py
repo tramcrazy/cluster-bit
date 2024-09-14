8563277e98a97c690e76adb66bcca4402a56033a
tramcrazy <tram@tramcrazy.com>
2024-09-14 20:24:16 +0100

    Add autodetection for micro:bits

---
+++ main.py
@@ -0,0 +1,18 @@
+import serial
+import serial.tools.list_ports
+import sys
+print("Welcome to cluster:bit!")
+print("Attempting autodetection of your micro:bit...")
+ports = serial.tools.list_ports.comports() # all serial ports on computer
+microbit_num = 0 # how many micro:bits are connected?
+for port in ports:
+    if "VID:PID=0D28:0204" in port.hwid: # checks each port to see if micro:bit vendor/product ID is present
+        microbit_port = port.device # stores the micro:bit's port for later
+        microbit_name = port.name # store friendlier name for later
+        microbit_num += 1
+if microbit_num == 0: # troubleshooting advice when no micro:bit is connected
+    print("It seems like your micro:bit isn't connected to this computer.\nMake sure it's connected with a USB data cable and run cluster:bit again.")
+    sys.exit()
+if microbit_num > 1: # troubleshooting advice when more than one micro:bit is connected
+    print("It seems like more than one micro:bit is connected to your computer.\ncluster:bit is designed to work with just one micro:bit over USB, and the others over radio.\nTry disconnecting any other micro:bits and run cluster:bit again.")
+print("A micro:bit has been detected on port " + microbit_name + ". Let's get clustering!")
