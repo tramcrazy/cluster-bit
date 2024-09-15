77af8e18c632cf385a441a78a700170a97784c5b
tramcrazy <tram@tramcrazy.com>
2024-09-15 17:29:21 +0100

    Update to encode as standard string

--- microbit_code/microbit_main.py
+++ microbit_code/microbit_main.py
@@ -1,9 +1,10 @@
 import microbit
 import utime
+
 def wait_for_task():
     while True:
         if microbit.uart.any():
-            received_text = microbit.uart.read()
+            received_text = str(microbit.uart.read(), "utf-8")
             if "TASK" in received_text:
                 break
         utime.sleep(1)
